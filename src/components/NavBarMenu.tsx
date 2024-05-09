cf6a5185f84be073352acb05cb6900b594d090ff
Philip Krück <hi@philipkrueck.com>
2024-05-09 20:55:21 +0200

    update nav bar

--- src/components/NavBarMenu.tsx
+++ src/components/NavBarMenu.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import {
   HStack,
   Button,
@@ -18,9 +19,39 @@ import { DarkModeSwitch } from "./DarkModeSwitch";
 export const NavBarMenu = () => {
   const router = useRouter();
 
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [scrolled, setScrolled] = useState(false);
+  const [isShown, setIsShown] = useState(true);
+
+  const checkScrolled = () => {
+    let st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st > lastScrollTop) {
+      setIsShown(false);
+      setScrolled(st > 0);
+    } else {
+      setIsShown(true);
+      setScrolled(st > 0);
+    }
+    setLastScrollTop(st <= 0 ? 0 : st);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", checkScrolled);
+    return () => window.removeEventListener("scroll", checkScrolled);
+  }, [lastScrollTop]);
+
   return (
-    <Box as="nav" w="100%" position="fixed" top={4} zIndex={2}>
-      <Flex justifyContent="center" alignItems="center">
+    <Box
+      as="nav"
+      w="100%"
+      position="fixed"
+      zIndex={2}
+      style={scrolled ? { backdropFilter: "blur(10px)" } : undefined}
+      transition="transform 0.3s ease-out"
+      transform={isShown ? "translateY(0)" : "translateY(-100%)"}
+      display={isShown ? "block" : "none"}
+    >
+      <Flex justifyContent="center" alignItems="center" my={4}>
         <DarkModeSwitch />
         <HStack>
           <MenuButton
