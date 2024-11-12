32161b71c58fa1f9e5381af3666bec6c4916b9af
Philip Krück <hi@philipkrueck.com>
2024-11-12 21:14:27 +0700

    disable omsplanner.pro

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -11,33 +11,33 @@ import {
 
 const Projects = () => (
   <Center>
-    <ChakraLink
-      href={"https://omsplanner.pro"}
-      textDecoration="none"
-      _hover={{ textDecoration: "none" }}
-    >
-      <Card
-        key={"omsplanner"}
-        mb="8"
-        maxW="sm"
-        _hover={{ transform: "scale(1.02)", transition: "0.2s" }}
-      >
-        <CardBody>
-          <Image
-            src="omsplanner-hero-image.png"
-            alt="Green double couch with wooden legs"
-            borderRadius="lg"
-          />
-          <Stack mt="6" spacing="3">
-            <Heading size="md">{"omsplanner.pro"}</Heading>
-            <Text>
-              I built a planner for Online Masters students at Georgia Tech to
-              optimally manage their studies in Notion.
-            </Text>
-          </Stack>
-        </CardBody>
-      </Card>
-    </ChakraLink>
+    {/* <ChakraLink */}
+    {/*   href={"https://omsplanner.pro"} */}
+    {/*   textDecoration="none" */}
+    {/*   _hover={{ textDecoration: "none" }} */}
+    {/* > */}
+    {/*   <Card */}
+    {/*     key={"omsplanner"} */}
+    {/*     mb="8" */}
+    {/*     maxW="sm" */}
+    {/*     _hover={{ transform: "scale(1.02)", transition: "0.2s" }} */}
+    {/*   > */}
+    {/*     <CardBody> */}
+    {/*       <Image */}
+    {/*         src="omsplanner-hero-image.png" */}
+    {/*         alt="Green double couch with wooden legs" */}
+    {/*         borderRadius="lg" */}
+    {/*       /> */}
+    {/*       <Stack mt="6" spacing="3"> */}
+    {/*         <Heading size="md">{"omsplanner.pro"}</Heading> */}
+    {/*         <Text> */}
+    {/*           I built a planner for Online Masters students at Georgia Tech to */}
+    {/*           optimally manage their studies in Notion. */}
+    {/*         </Text> */}
+    {/*       </Stack> */}
+    {/*     </CardBody> */}
+    {/*   </Card> */}
+    {/* </ChakraLink> */}
   </Center>
 );
 
