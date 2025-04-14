4411dcee2e9a479db949200d89341349e2d05e86
Philip Krück <hi@philipkrueck.com>
2025-04-14 14:01:44 -0300

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,6 @@ import { Text } from "@chakra-ui/react";
 
 export const Footer = () => (
   <Text pt={20} fontSize={"xs"} textAlign={"center"}>
-    Thanks for visiting. © 2024. Stay curious, keep coding. 🚀
+    Thanks for visiting. © 2025. Stay curious, keep coding. 🚀
   </Text>
 );
