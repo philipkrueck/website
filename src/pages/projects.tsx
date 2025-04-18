6971262774a6c9b7df2343fd00324b6694411372
Philip Krück <hi@philipkrueck.com>
2025-04-18 15:33:16 -0300

    add solstamp.io

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -11,6 +11,33 @@ import {
 
 const Projects = () => (
   <Center>
+    <ChakraLink
+      href={"https://solstamp.io"}
+      textDecoration="none"
+      _hover={{ textDecoration: "none" }}
+    >
+      <Card
+        key={"solstamp"}
+        mb="8"
+        maxW="sm"
+        _hover={{ transform: "scale(1.02)", transition: "0.2s" }}
+      >
+        <CardBody>
+          {/* <Image */}
+          {/*   src="omsplanner-hero-image.png" */}
+          {/*   alt="solstamp" */}
+          {/*   borderRadius="lg" */}
+          {/* /> */}
+          <Stack mt="6" spacing="3">
+            <Heading size="md">{"solstamp.io"}</Heading>
+            <Text>
+              I'm building a collection of no-code solana tools including a NFT
+              creator and token minter.
+            </Text>
+          </Stack>
+        </CardBody>
+      </Card>
+    </ChakraLink>
     {/* <ChakraLink */}
     {/*   href={"https://omsplanner.pro"} */}
     {/*   textDecoration="none" */}
