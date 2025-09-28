0967f72c417d61b9468b4e8c080271bf8fd0bcaa
Philip Krück <hi@philipkrueck.com>
2025-09-28 14:14:11 +0100

    feat: remove solstamp from projects section

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -29,7 +29,7 @@ const Project = ({ name, description, image, link }: ProjectProps) => (
       _hover={{ transform: "scale(1.02)", transition: "0.2s" }}
     >
       <CardBody>
-        <Image src={image} alt="solstamp" borderRadius="lg" />
+        <Image src={image} alt={name} borderRadius="lg" />
         <Stack mt="6" spacing="3">
           <Heading size="md">{name}</Heading>
           <Text>{description}</Text>
@@ -42,22 +42,16 @@ const Project = ({ name, description, image, link }: ProjectProps) => (
 const Projects = () => (
   <Grid templateColumns="repeat(2, 1fr)" gap="6">
     <Project
-      name=".dotfiles"
-      description="My personal dotfiles will likely be the longest living project throughout my career."
-      image="dotfiles.png"
-      link="https://github.com/philipkrueck/.dotfiles"
-    />
-    <Project
-      name="solstamp.io"
-      description="A collection of open-source no-code Solana tools including a fungible and non-fungible token minter."
-      image="solstamp.png"
-      link="https://solstamp.io"
-    />
-    <Project
       name="homelab"
       description="Kubernetes homelab hosting a variety of applications acting as a learning ground for my explorations into the K8s universe."
       image="homelab.png"
       link="https://github.com/philipkrueck/homelab"
+    />
+    <Project
+      name=".dotfiles"
+      description="My personal dotfiles will likely be the longest living project throughout my career."
+      image="dotfiles.png"
+      link="https://github.com/philipkrueck/.dotfiles"
     />
   </Grid>
 );
